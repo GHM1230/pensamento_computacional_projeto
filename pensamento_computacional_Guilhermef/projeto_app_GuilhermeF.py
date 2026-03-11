9aae8332dac32481ef94cac5f63e6e5f3af1f08a
Guilherme Firmino da Silva. <guilhermefsilva.09012009@gmail.com>
2026-03-11 18:01:44 -0300

    Add files via upload

--- pensamento_computacional_Guilhermef/projeto_app_GuilhermeF.py
+++ pensamento_computacional_Guilhermef/projeto_app_GuilhermeF.py
@@ -59,36 +59,3 @@ print('Turma A 8:00 - 12:00')
 print('')
 print('Turma A 10:00 - 12:00')
 
-'''
-
-CRUD
-
-calculadora simples - Python Vocação
-
-'''
-
-print('\n Calculadora simples - Python Vocação \n')
-
-number_um = input('digite o primeiro número :')
-number_dois = input('digite o segundo número :')
-
-operar_number = input('Escolha a operacao: 1 -> +, 2 -> -, 3 -> *, 4 -> /')
-
-if operar_number == '4':
-    result = int(number_um) / int(number_dois)
-    print(f'O resultado é: {result}')
-
-elif operar_number == '3':
-    result = int(number_um) * int(number_dois)
-    print(f'O resultado é: {result}')
-
-elif operar_number == '2':
-    result = int(number_um) - int(number_dois)
-    print(f'O resultado é: {result}')
-
-elif operar_number == '1':
-    result = int(number_um) + int(number_dois)
-    print(f'O resultado é: {result}')
-
-else:
-    print('número não divido por zero.')
